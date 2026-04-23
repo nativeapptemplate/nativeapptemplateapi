a8dd3b78b1c2be3b34978bb5f8ea6da970df62e1
dadachi <maurois@mac.com>
2026-04-24 08:54:23 +0900

    Update serializers for new ItemTag schema
    ItemTagSerializer:
    - Drop queue_number, scan_state, customer_read_at, already_completed
    - Add name, description, position
    
    ShopSerializer:
    - Drop scanned_item_tags_count (referenced removed scan_state)
    - Drop display_shop_server_path (display route removed)

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -11,15 +11,7 @@ class ShopSerializer
     shop.item_tags.size
   end
 
-  attribute :scanned_item_tags_count do |shop|
-    shop.item_tags.scanned.size
-  end
-
   attribute :completed_item_tags_count do |shop|
     shop.item_tags.completed.size
-  end
-
-  attribute :display_shop_server_path do |shop|
-    Rails.application.routes.url_helpers.display_shop_path(shop, type: "server")
   end
 end
